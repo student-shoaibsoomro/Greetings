1ea75684bd34fb34b4561e373ae8a6150eadbd79
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-04-28 03:45:38 +0500

    Add files via upload

---
+++ greetings.js
@@ -0,0 +1,5 @@
+"use strict";
+let Names = ["Shoaib", "Haris", "Salman"];
+console.log(`They are my best friends ${Names[0]}`);
+console.log(`${Names[1]}, is my best friend`);
+console.log(`I really like him ${Names[2]}`);
